7f0e5f2ebf70a9fdfca5604cac25a1da582842fc
Victor Hugo Vieira <victor.h.souza.vieira@gmail.com>
2020-05-29 19:17:47 -0300

    Adiciona testes de integração para cenário de erro na adição de itens no estoque

--- spec/requests/api/v1/stock_items_request_spec.rb
+++ spec/requests/api/v1/stock_items_request_spec.rb
@@ -36,5 +36,41 @@ RSpec.describe 'StockItems', type: :request do
         expect(response).to have_http_status :created
       end
     end
+
+    context 'with invalid parameters' do
+
+      it 'when you submit the invalid product_id' do
+
+        valid_attributes[:product_id] = -1
+
+        post api_v1_stock_items_add_item_url, params: {
+                                                stock_item: valid_attributes
+                                              },
+                                              headers: valid_headers, as: :json
+
+        response_body = json_parser(response.body)
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(response_body['error']['message']).to eql I18n.t('activerecord.errors.messages.record_invalid',
+                                                                errors: I18n.t('stock_items.errors.invalid_product'))
+      end
+
+      it 'when you submit the invalid quantity' do
+
+        valid_attributes[:quantity] = -3
+
+        post api_v1_stock_items_add_item_url, params: {
+                                                stock_item: valid_attributes
+                                              },
+                                              headers: valid_headers, as: :json
+
+        response_body = json_parser(response.body)
+
+        expect(response).to have_http_status :unprocessable_entity
+        expect(response_body['error']['message']).to eql I18n.t('activerecord.errors.messages.record_invalid',
+                                                                errors: I18n.t('stock_items.errors.invalid_quantity'))
+      end
+    end
   end
+  
 end
